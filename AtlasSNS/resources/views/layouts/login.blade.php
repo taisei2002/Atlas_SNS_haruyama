3135909de98d1c109d0142ab1a439d3015880213
taisei2002 <kaeda1880@gmail.com>
2022-05-01 17:33:35 +0900

    no message

---
+++ AtlasSNS/resources/views/layouts/login.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+    <!--IEブラウザ対策-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="description" content="ページの内容を表す文章" />
+    <title></title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }} ">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }} ">
+    <!--スマホ,タブレット対応-->
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <!--サイトのアイコン指定-->
+    <link rel="icon" href="images/icon1.png" sizes="16x16" type="image/png" />
+    <link rel="icon" href="images/icon1.png" sizes="32x32" type="image/png" />
+    <link rel="icon" href="images/icon1.png" sizes="48x48" type="image/png" />
+    <link rel="icon" href="images/icon1.png" sizes="62x62" type="image/png" />
+    <!--iphoneのアプリアイコン指定-->
+    <link rel="apple-touch-icon-precomposed" href="images/icon1.png" />
+    <!--OGPタグ/twitterカード-->
+</head>
+<body>
+    <header>
+        <div id = "head">
+        <h1 class= "users"><a href="/top" ><img src="images/atlas.png"></a></h1>
+            <div id="">
+                <div id="">
+                    <p>〇〇さん<a href="/profile" ><img src="images/icon1.png"></p>
+                <div>
+  <ul id="accordion" class="accordion">
+      <div class="link"><i class="fa fa-paint-brush"></i>^<i class="fa fa-chevron-down"></i></div>
+
+                <ul class="submenu">
+                    <li><a href="/top">HOME</a></li>
+                    <li><a href="/profile">プロフィール</a></li>
+                    <li><a href="/logout">ログアウト</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div id="row">
+        <div id="container">
+            @yield('content')
+        </div >
+        <div id="side-bar">
+            <div id="confirm">
+                <p>〇〇さんの</p>
+                <div>
+                <p>フォロー数</p>
+                <p>〇〇名</p>
+                </div>
+                <p class="btn"><a href="">フォローリスト</a></p>
+                <div>
+                <p>フォロワー数</p>
+                <p>〇〇名</p>
+                </div>
+                <p class="btn"><a href="">フォロワーリスト</a></p>
+            </div>
+            <p class="btn"><a href="">ユーザー検索</a></p>
+        </div>
+    </div>
+    <footer>
+    </footer>
+    <script src="JavaScriptファイルのURL"></script>
+    <script src="JavaScriptファイルのURL"></script>
+</body>
+</html>
+
