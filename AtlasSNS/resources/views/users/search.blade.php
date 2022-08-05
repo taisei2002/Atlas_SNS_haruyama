92a3ba80c707413aed47628084732a61fe822008
taisei2002 <kaeda1880@gmail.com>
2022-08-05 23:22:17 +0900

    サーチblade更新

--- AtlasSNS/resources/views/users/search.blade.php
+++ AtlasSNS/resources/views/users/search.blade.php
@@ -4,18 +4,25 @@
 {!! Form::open(['method' => 'GET']) !!}
     {!! Form::text('keyword', null) !!}
     {!! Form::submit('検索') !!}
+
+検索ワード：「 {{$keyword}} 」
+
 {!! Form::close() !!}
-
-
     @foreach ($user as $user)
-<h3>{{ $user->username }}</h3>
+<h3>{{ $user->username }}
+     <div class="d-flex justify-content-end flex-grow-1">
+         <form action="{{ url('search/follow/{id}') }}" method="POST">
+      {{ csrf_field() }}
+      <button type="submit" class="btn btn-primary btn-sm">フォローする</button>
+    </form>
+    <form action="{{ url('search/un_follow/{id}') }}" method="POST">
+      {{ csrf_field() }}
+      {{ method_field('') }}
+      <button type="submit" class="btn btn-danger btn-sm">フォロー解除</button>
+    </form>
+</div>
+</h3>
+
 
  @endforeach
-
-
-
-  </>
-</div>
-
-
 @endsection
