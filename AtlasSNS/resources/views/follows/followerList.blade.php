0f4a0a7c7ae6c789710f920f57a1f715ce2a7f15
taisei2002 <kaeda1880@gmail.com>
2022-09-01 21:53:33 +0900

    フォロー、フォロワーリスト

--- AtlasSNS/resources/views/follows/followerList.blade.php
+++ AtlasSNS/resources/views/follows/followerList.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.login')
 
 @section('content')
-<h2>フォロワーリスト</h2>
+
+{!! Form::close() !!}
+@foreach ($user as $user)
+<!--ユーザー以外表示-->
+@if ($user->id !== Auth::user()->id)
+
+
+<div>{{ $user->username }}
+<div class="container">
+  <div class="row justify-content-center">
+   <div class="col-md-8">
+    <div class="ml-2 d-flex flex-column">
+<!--フォローボタン-->
+@if (auth()->user()->isFollowing($user->id))
+     <form action="{{ route('unfollow', ['id' => $user->id]) }}" method="POST">
+         {{ csrf_field() }}
+        {{ method_field('DELETE') }}
+         <button type="submit" class="btn btn-danger btn-sm">フォロー解除</button>
+     </form>
+       @else
+     <form action="{{ url('/search/follow/'.$user->id) }}" method="POST">
+        {{ csrf_field() }}
+        <button type="submit" class="btn btn-primary btn-sm">フォローする</button>
+
+     </form>
+         @endif
+    </div>
+   </div>
+  </div>
+ </div>
+</div>
+
+@endif
+@endforeach
 @endsection
